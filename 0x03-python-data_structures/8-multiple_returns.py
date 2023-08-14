1278ccda2ac65415b1db6d67471d1330b76c1d68
Doctorstrange <doctorstrange247@gmail.com>
2023-08-14 20:00:30 +0100

    8. More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    the_list = list(sentence)
+    if the_list[0] == "":
+        return (None)
+    mul_tuple = (len(sentence), the_list[0])
+    return (mul_tuple)
