eddf74e493cb3ad1086f12c17ccb3c85a13d715c
Doctorstrange <doctorstrange247@gmail.com>
2023-08-14 20:05:46 +0100

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     the_list = list(sentence)
-    if the_list[0] == "":
+    if the_list == "":
         return (0, None)
     mul_tuple = (len(sentence), the_list[0])
     return (mul_tuple)
