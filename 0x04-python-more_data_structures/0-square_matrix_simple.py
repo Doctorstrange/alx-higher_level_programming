c021c92d60e79f15f20e6fd919f72c21d4c3b77f
Doctorstrange <doctorstrange247@gmail.com>
2023-08-16 20:30:33 +0100

    Write a function that computes the square value of all integers of a matrix.

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,5 +6,5 @@ def square_matrix_simple(matrix=[]):
         index = 0
         for column in row:
             matrix_copy[index][count] = column**2
-            count =+ 1
+            count = + 1
     return (matrix_copy)
