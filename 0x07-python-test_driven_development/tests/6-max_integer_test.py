328a5ec8791df8d8a903a0a6c7d287494bb388fa
Doctorstrange <doctorstrange247@gmail.com>
2023-09-09 03:53:26 +0100

    Since the beginning you have been creating “Interactive tests”. For this exercise, you will add Unittests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -24,16 +24,10 @@ class TestMaxInteger(unittest.TestCase):
 		one_element = [5]
 		self.assertEqual(max_integer(one_element), 5)
 
-	def test_ints_and_floats(self):
-		"""list of ints and floats."""
-		ints_and_floats = [1.53, 15.5, -9, 15, 6]
-		self.assertEqual(max_integer(ints_and_floats), 15.5)
-
-
 	def test_string(self):
 		"""Test a string."""
-		string = "Brennan"
-		self.assertEqual(max_integer(string), 'r')
+		string = "femi"
+		self.assertEqual(max_integer(string), 'e')
 
 
 	def test_empty_string(self):
