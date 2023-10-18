a62a1de41b8b61aa10477265e912a4cf49481cb0
Doctorstrange <Doctorstrange>
2023-10-18 17:36:07 -0500

    script that creates the MySQL server user user_0d_1.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- script that creates the MySQL server user user_0d_1.
+-- script that creates the MySQL server user user_0d_1
 CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
