f681408515026f523b4762f539b63e4662412535
Doctorstrange <Doctorstrange>
2023-11-14 04:06:26 +0100

    Write a function that returns the addition of 2 integers.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const check = process.argv.slice(2).map(Number);
-  const second_biggest = check.sort(function (a, b) { return b - a; })[1];
-  console.log(second_biggest);
+  const second_biggest = check.sort(function (x, y) { return y - x; })[1];
+  console.log(second);
 }
