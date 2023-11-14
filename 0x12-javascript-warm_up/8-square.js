21c83474edfd97ae1b714a2b6f2bc28bd86eb31b
Doctorstrange <Doctorstrange>
2023-11-14 03:51:23 +0100

    Write a script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const length = Number(process.argv[2]);
+  let breath = 0;
+  while (breath < x) {
+    console.log('X'.repeat(length));
+    breath++;
+  }
+}
