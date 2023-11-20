70da9b25947bbca377cae5fbc23f6c03d99c5fb1
Doctorstrange <Doctorstrange>
2023-11-19 21:00:17 -0600

    o model_state.py named model_city.py

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""contains the class definition of a City"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for snap in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(snap[0] + ": (" + str(snap[1]) + ") " + snap[2])
