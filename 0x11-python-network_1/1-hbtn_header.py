76ba29d8066d1c9c2c686b8f6f760dd487ff533f
Doctorstrange <Doctorstrange>
2023-12-01 17:04:38 -0600

    1. Response header value #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" sends a request to the URL and displays the value of the X-Request-Id"""
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        inhead = response.info()
+        for arg in inhead._headers:
+            if arg[0] == 'X-Request-Id':
+                print(arg[1])
