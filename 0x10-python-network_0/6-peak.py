1571372a68834070765968e9f12c300c5270fd09
Doctorstrange <Doctorstrange>
2023-11-30 18:52:33 -0600

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that finds a peak in a list of unsorted integers."""
+def find_peak(list_of_integers):
+    """function that finds a peak in a list of unsorted integers."""
+    peak  = None
+    for value in list_of_integers:
+        if peak is None or peak < value:
+            peak = value
+    return peak
