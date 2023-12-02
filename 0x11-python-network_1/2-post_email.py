ca625ef482964fb142f1003955030aa05cd74368
Doctorstrange <Doctorstrange>
2023-12-01 18:45:15 -0600

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email, sends a POST"""
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+    url = sys.argv[1]
+    email = sys.argv[2]
+    inhead = {'email': email}
+    inhead = urllib.parse.urlencode(inhead)
+    inhead = payload.encode('ascii')
+    req = urllib.request.Request(url, inhead)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
